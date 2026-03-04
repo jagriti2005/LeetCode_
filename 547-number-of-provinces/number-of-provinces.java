342e479febc98f6517d1aee5bbfb5bff55e174a0
jagriti2005 <154238507+jagriti2005@users.noreply.github.com>
2026-03-04 22:49:28 +0530

    Time: 2 ms (46.35%) | Memory: 47.1 MB (87.89%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,40 +1,30 @@
 class Solution {
-    // public void bfs(int i, boolean[] vis, int[][] adj){
-    //     int n = adj.length;
-    //     vis[i] = true;
-    //     Queue<Integer> q = new LinkedList<>();
-    //     q.add(i);
-    //     while(q.size() > 0){
-    //         int front = q.remove();
-    //         for(int j=0;j<n;j++){
-    //             if(adj[front][j] == 1 && vis[j]==false){
-    //                 q.add(j);
-    //                 vis[j] = true;
-    //             }
-    //         }
-    //     }
-    // }
-
-    public void dfs(int i, boolean[] vis, int[][] adj){
-        int n = adj.length;
-        vis[i] = true;
-        for(int j=0;j<n;j++){
-            if(adj[i][j]==1 && vis[j]==false){
-                dfs(j,vis,adj);
-            }
+    static int[] parent;
+    public int find(int a){
+        if(parent[a] == a) return a;
+        return find(parent[a]);
+    }
+    public void union(int a,int b){
+        int leaderA = find(a);
+        int leaderB = find(b);
+        if(leaderA != leaderB){
+            parent[leaderB] = leaderA;
         }
     }
-
-    public int findCircleNum(int[][] adj) {
-        int n = adj.length;
-        int count = 0;
-        boolean[] vis = new boolean[n];
+    public int findCircleNum(int[][] isConnected) {
+        int n = isConnected.length;
+        parent = new int[n+1];
+        for(int i=1;i<=n;i++){
+            parent[i] = i;
+        }
         for(int i=0;i<n;i++){
-            if(!vis[i]){
-                // bfs(i,vis,adj);
-                dfs(i,vis,adj);
-                count++;
+            for(int j=0;j<n;j++){
+                if(i!=j && isConnected[i][j] == 1) union(i+1,j+1);
             }
+        }
+        int count = 0;
+        for(int i=1;i<=n;i++){
+            if(parent[i] == i) count++;
         }
         return count;
     }
