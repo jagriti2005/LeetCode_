5e5e5b44e07623855aa6234aa5f213367cdc029d
jagriti2005 <154238507+jagriti2005@users.noreply.github.com>
2026-03-06 01:21:28 +0530

    Time: 505 ms (31.44%) | Memory: 76.5 MB (71.89%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.java
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.java
@@ -1,32 +1,60 @@
 class Solution {
-    class Pair{
-        int node;
-        int weight;
-        Pair(int node,int weight){
-            this.node = node;
-            this.weight = weight;
+    class Tuple{
+        int u,v,dist;
+        Tuple(int u,int v,int dist){
+            this.u = u;
+            this.v = v;
+            this.dist = dist;
         }
     }
+
+    static int[] parent,size;
+
+    public int find(int a){
+        if(parent[a] == a) return a;
+        return parent[a] = find(parent[a]);
+    }
+
+    public void union(int a,int b){
+        a = find(a);
+        b = find(b);
+        if(size[a] > size[b]){
+            parent[b] = a;
+            size[a] += size[b];
+        }
+        else{
+            parent[a] = b;
+            size[b] += size[a];
+        }
+    }
+
     public int minCostConnectPoints(int[][] points) {
-        boolean[] vis = new boolean[points.length];
-        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b)->a.weight-b.weight);
-        pq.add(new Pair(0,0));
-        int cost = 0;
-        while(pq.size()>0){
-            Pair top = pq.poll();
-            if(!vis[top.node]){
-                cost += top.weight;
-                vis[top.node] = true;
-            }
-            else continue;
-            for(int i=0;i<points.length;i++){
-                if(i==top.node) continue;
-                int x1 = points[top.node][0], y1 = points[top.node][1];
-                int x2 = points[i][0], y2 = points[i][1];
+        int n = points.length;
+
+        parent = new int[n];
+        size = new int[n];
+        for(int i=0;i<n;i++){
+            parent[i] = i;
+            size[i] = 1;
+        }
+        
+        PriorityQueue<Tuple> pq = new PriorityQueue<>((a,b)->a.dist-b.dist);
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                int x1 = points[i][0], y1 = points[i][1];
+                int x2 = points[j][0], y2 = points[j][1];
                 int dist = Math.abs(x1-x2) + Math.abs(y1-y2);
-                pq.add(new Pair(i,dist));
+                pq.add(new Tuple(i,j,dist));
             }
         }
-        return cost;
+        int minCost = 0;
+        while(pq.size()>0){
+            Tuple top = pq.poll();
+            if(find(top.u) != find(top.v)){
+                minCost += top.dist;
+                union(top.u,top.v);
+            }
+        }
+        return minCost;
     }
 }
