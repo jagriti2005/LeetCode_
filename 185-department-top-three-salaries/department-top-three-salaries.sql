7c3758e998065926d46b03bfec27bb078b974bef
jagriti2005 <154238507+jagriti2005@users.noreply.github.com>
2026-03-11 12:03:00 +0530

    Time: 1310 ms (16.79%) | Memory: 0B (100.00%) - LeetSync

--- 185-department-top-three-salaries/department-top-three-salaries.sql
+++ 185-department-top-three-salaries/department-top-three-salaries.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-select d.name as Department, e.name as Employee, e.salary from employee e
+select d.name as Department, e.name as Employee, e.salary as Salary from employee e
 join department d on e.departmentId = d.id
 where (
     select count(distinct e2.salary) 
