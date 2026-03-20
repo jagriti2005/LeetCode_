80acc90bb252b75de0688bee0a5d272164ef46cc
jagriti2005 <154238507+jagriti2005@users.noreply.github.com>
2026-03-21 00:15:48 +0530

    Time: 28 ms (38.30%) | Memory: 47.7 MB (53.19%) - LeetSync

--- 3884-minimum-absolute-difference-in-sliding-submatrix/minimum-absolute-difference-in-sliding-submatrix.java
+++ 3884-minimum-absolute-difference-in-sliding-submatrix/minimum-absolute-difference-in-sliding-submatrix.java
@@ -5,8 +5,6 @@ class Solution {
         int m = grid.length;
         int n = grid[0].length;
         int[][] ans = new int[m - k + 1][n - k + 1];
-        
-        // For each possible top-left corner
         for (int i = 0; i <= m - k; i++) {
             for (int j = 0; j <= n - k; j++) {
                 ans[i][j] = getMinDifference(grid, i, j, k);
@@ -17,33 +15,23 @@ class Solution {
     }
     
     private int getMinDifference(int[][] grid, int startRow, int startCol, int k) {
-        // Use TreeSet to maintain sorted distinct values
         TreeSet<Integer> distinctValues = new TreeSet<>();
-        
-        // Collect all distinct values in the k×k submatrix
         for (int i = 0; i < k; i++) {
             for (int j = 0; j < k; j++) {
                 distinctValues.add(grid[startRow + i][startCol + j]);
             }
         }
-        
-        // If only one distinct value, answer is 0
-        if (distinctValues.size() == 1) {
-            return 0;
-        }
-        
-        // Find minimum absolute difference between consecutive distinct values
+        if (distinctValues.size() == 1)  return 0;
         int minDiff = Integer.MAX_VALUE;
         Integer prev = null;
         
         for (int val : distinctValues) {
             if (prev != null) {
-                int diff = val - prev; // Since TreeSet is sorted, val > prev
+                int diff = val - prev;
                 minDiff = Math.min(minDiff, diff);
             }
             prev = val;
-        }
-        
+        }    
         return minDiff;
     }
 }
