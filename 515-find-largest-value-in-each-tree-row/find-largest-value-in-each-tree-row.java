1f1bd960709a28372cf542a7415eaffb4b513a48
jagriti2005 <154238507+jagriti2005@users.noreply.github.com>
2026-05-01 21:15:26 +0530

    Time: 4 ms (7.36%) | Memory: 47.7 MB (14.14%) - LeetSync

---
+++ 515-find-largest-value-in-each-tree-row/find-largest-value-in-each-tree-row.java
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root){
+        List<List<Integer>> res = new ArrayList<>();
+        if(root == null) return res;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        while(q.size() > 0){
+            int size = q.size();
+            List<Integer> list = new ArrayList<>();
+            for(int i=0;i<size;i++){
+                TreeNode curr = q.poll();
+                list.add(curr.val);
+                if(curr.left != null) q.add(curr.left);
+                if(curr.right != null) q.add(curr.right);
+            }
+            res.add(list);
+        }
+        return res;
+    }
+    public List<Integer> largestValues(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        List<List<Integer>> list = levelOrder(root);
+        for(List<Integer> l : list){
+            int max = Integer.MIN_VALUE;
+            for(int i=0;i<l.size();i++){
+                max = Math.max(max, l.get(i));
+            }
+            ans.add(max);
+        }
+        return ans;
+    }
+}
