9e3b4c55d1095dd61d6efad9b957d79fbbd63339
jagriti2005 <154238507+jagriti2005@users.noreply.github.com>
2026-05-12 00:10:10 +0530

    Time: 28 ms (17.07%) | Memory: 57.5 MB (10.91%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -1,12 +1,16 @@
 class Solution {
+    public int ways(int amount, int[] coins, int i, int[][] dp){
+        if(amount == 0) return 1;
+        if(i==coins.length) return 0;
+        if(dp[i][amount] != -1) return dp[i][amount];
+        int skip = ways(amount,coins,i+1,dp);
+        int take = 0;
+        if(amount >= coins[i]) take = ways(amount-coins[i],coins,i,dp);
+        return dp[i][amount] = take + skip;
+    }
     public int change(int amount, int[] coins) {
-        int[] dp = new int[amount+1];
-        dp[0] = 1;
-        for(int coin:coins){
-            for(int i=coin; i<= amount;i++){
-                dp[i] += dp[i-coin];
-            }
-        }
-        return dp[amount];
+        int[][] dp = new int[coins.length][amount+1];
+        for(int[] arr:dp) Arrays.fill(arr,-1);
+        return ways(amount, coins, 0, dp);
     }
 }
