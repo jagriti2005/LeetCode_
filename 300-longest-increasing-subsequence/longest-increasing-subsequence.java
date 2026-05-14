5231a976f8c52ab3a1f51b80baeaeae9ac5afe46
jagriti2005 <154238507+jagriti2005@users.noreply.github.com>
2026-05-14 17:05:48 +0530

    Time: 5 ms (88.85%) | Memory: 46.2 MB (54.72%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,32 +1,28 @@
-// class Solution {
-//     public int lengthOfLIS(int[] nums) {
-//         int n = nums.length;
-//         int[][] dp = new int[n+1][n+1];
-//         for(int i=n-1;i>=0;i--){
-//             for(int j=i-1;j>=-1;j--){
-//                 int take = 0;
-//                 if(j==-1 || nums[i] > nums[j]) take = 1 + dp[i+1][i+1];
-//                 int skip = dp[i+1][j+1];
-//                 dp[i][j+1] = Math.max(take,skip);
-//             }
-//         }
-//         return dp[0][0];
-//     }
-// }
-
-
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int n = nums.length;
-        int[] dp = new int[n];
-        int maxLen = 0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<i;j++){
-                if(nums[i] > nums[j]) dp[i] = Math.max(dp[i],dp[j]);
+        ArrayList<Integer> ans = new ArrayList<>();
+
+        for (int num : nums) {
+            int lo = 0;
+            int hi = ans.size() - 1;
+
+            while (lo <= hi) {
+                int mid = lo + (hi - lo) / 2;
+
+                if (ans.get(mid) < num) {
+                    lo = mid + 1;
+                } else {
+                    hi = mid - 1;
+                }
             }
-            dp[i] += 1;
-            maxLen = Math.max(maxLen,dp[i]);
+
+            if (lo == ans.size()) {
+                ans.add(num);
+            } else {
+                ans.set(lo, num);
+            }
         }
-        return maxLen;
+
+        return ans.size();
     }
 }
