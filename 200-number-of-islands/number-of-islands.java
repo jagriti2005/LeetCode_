b835e1465dc5bd90aa3bd14bfa34bdaaaffb7c88
jagriti2005 <154238507+jagriti2005@users.noreply.github.com>
2026-05-17 22:47:30 +0530

    Time: 4 ms (43.00%) | Memory: 52.2 MB (68.82%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,49 +1,29 @@
 class Solution {
-    class Pair{
-        int r,c;
-        Pair(int r, int c){
-            this.r = r;
-            this.c = c;
-        }
-    }
-
-    int[] rowD = {0,0,-1,1};
-    int[] colD = {-1,1,0,0};
-
-    public void bfs(int i, int j, char[][] grid, boolean[][] vis){
+    public void dfs(char[][] grid, int i, int j, boolean[][] vis){
         int n = grid.length;
         int m = grid[0].length;
 
-        Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(i,j));
-        vis[i][j] = true;
+        if(i<0 || i>= n || j<0 || j>=m || grid[i][j] != '1' || vis[i][j]) return;
         
-        while(q.size() > 0){
-            Pair curr = q.poll();
-            int r = curr.r;
-            int c = curr.c;
-            for(int k=0;k<4;k++){
-                int nr = rowD[k] + r;
-                int nc = colD[k] + c;
+        vis[i][j] = true;
 
-                if(nr >= 0 && nr<n && nc>=0 && nc<m && grid[nr][nc] == '1' && !vis[nr][nc]){
-                    q.add(new Pair(nr,nc));
-                    vis[nr][nc] = true;
-                }
-            }
-        }
+        dfs(grid,i+1,j,vis);
+        dfs(grid,i,j+1,vis);
+        dfs(grid,i-1,j,vis);
+        dfs(grid,i,j-1,vis);
     }
     public int numIslands(char[][] grid) {
-        int count = 0;
         int n = grid.length;
         int m = grid[0].length;
         boolean[][] vis = new boolean[n][m];
+        
+        int count = 0;
 
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j] == '1' && !vis[i][j]){
-                    bfs(i,j,grid,vis);
                     count++;
+                    dfs(grid,i,j,vis);
                 }
             }
         }
