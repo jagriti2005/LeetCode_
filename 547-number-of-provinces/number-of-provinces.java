0307e9a40ded1149ab50b3689b55eb3924fe6d92
jagriti2005 <154238507+jagriti2005@users.noreply.github.com>
2026-05-18 11:06:19 +0530

    Time: 1 ms (87.00%) | Memory: 47.3 MB (41.92%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,40 +1,19 @@
 class Solution {
-    static int[] parent;
-    static int[] size;
-    public int find(int a){
-        if(parent[a] == a) return a;
-        return parent[a] = find(parent[a]);
-    }
-    public void union(int a,int b){
-        a = find(a);
-        b = find(b);
-        if(a != b){
-            if(size[a] > size[b]){
-                parent[b] = a;
-                size[a] += size[b];
-            }
-            else{
-                parent[a] = b;
-                size[b] += size[a];
-            }
+    public void dfs(int[][] adj, int i, boolean[] vis){
+        vis[i] = true;
+        for(int j=0;j<adj.length;j++){
+            if(adj[i][j] == 1 && !vis[j]) dfs(adj,j,vis);
         }
     }
-    public int findCircleNum(int[][] isConnected) {
-        int n = isConnected.length;
-        parent = new int[n+1];
-        size = new int[n+1];
-        for(int i=1;i<=n;i++){
-            parent[i] = i;
-            size[i] = 1;
-        }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(i!=j && isConnected[i][j] == 1) union(i+1,j+1);
-            }
-        }
+    public int findCircleNum(int[][] adj) {
+        int n = adj.length;
         int count = 0;
-        for(int i=1;i<=n;i++){
-            if(parent[i] == i) count++;
+        boolean[] vis = new boolean[n];
+        for(int i=0;i<n;i++){
+            if(!vis[i]){
+                dfs(adj,i, vis);
+                count++;
+            }
         }
         return count;
     }
