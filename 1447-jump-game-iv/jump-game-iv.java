7a46010a64cd9147edb6355acf459c0eaff7cc66
jagriti2005 <154238507+jagriti2005@users.noreply.github.com>
2026-05-29 11:16:44 +0530

    Time: 59 ms (42.93%) | Memory: 81.4 MB (49.37%) - LeetSync

--- 1447-jump-game-iv/jump-game-iv.java
+++ 1447-jump-game-iv/jump-game-iv.java
@@ -1,12 +1,8 @@
 class Solution {
     public int minJumps(int[] arr) {
-
         int n = arr.length;
-
         if(n == 1) return 0;
-
         HashMap<Integer, List<Integer>> map = new HashMap<>();
-
         for(int i = 0; i < n; i++){
             if(!map.containsKey(arr[i])){
                 map.put(arr[i], new ArrayList<>());
@@ -16,62 +12,38 @@ class Solution {
         }
 
         Queue<Integer> q = new LinkedList<>();
-        boolean[] visited = new boolean[n];
+        boolean[] vis = new boolean[n];
 
         int steps = 0;
+        q.add(0);
+        vis[0] = true;
 
-        q.offer(0);
-        visited[0] = true;
-
-        while(!q.isEmpty()){
-            
+        while(q.size() > 0){
             int len = q.size();
-
-            for(int i = 0; i < len; i++){
+            while(len-- > 0){
                 int curr = q.poll();
+                if(curr == n-1) return steps;
 
-                if(curr == n - 1) return steps;
-
-                //forward
-
-                if(curr + 1 < n && !visited[curr+1]){
-                    q.offer(curr + 1);
-                    visited[curr+1] = true;
+                if(curr+1<n && !vis[curr+1]){
+                    q.add(curr+1);
+                    vis[curr+1] = true;
                 }
 
-                   //backward
-
-                if(curr - 1 >= 0 && !visited[curr - 1]){
-                    q.offer(curr - 1);
-                    visited[curr - 1] = true;
+                if(curr-1>=0 && !vis[curr-1]){
+                    q.add(curr-1);
+                    vis[curr-1] = true;
                 }
-
-                //same value
 
                 for(int same : map.get(arr[curr])){
-                    if(!visited[same]){
-                        q.offer(same);
-                        visited[same] = true;
+                    if(!vis[same]){
+                        q.add(same);
+                        vis[same] = true;
                     }
                 }
-
                 map.get(arr[curr]).clear();
-
-
             }
             steps++;
-            
-
         }
-        
-        
-
-        return -1;
-
-
-
-        
-    }
-    
-    
+        return -1;     
+    } 
 }
