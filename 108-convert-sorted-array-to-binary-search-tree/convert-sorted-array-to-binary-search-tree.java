eec75ec9e2a46b2480fb753de389db4ee240fd23
jagriti2005 <154238507+jagriti2005@users.noreply.github.com>
2026-06-08 14:27:07 +0530

    Time: 0 ms (100.00%) | Memory: 45 MB (43.68%) - LeetSync

--- 108-convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.java
+++ 108-convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.java
@@ -14,11 +14,11 @@
  * }
  */
 class Solution {
-    public TreeNode build(int[] nums, int left, int right){
+    public TreeNode build(int[] nums,int left, int right){
         if(left > right) return null;
-        int mid = left + (right-left)/2;
+        int mid = left+(right-left)/2;
         TreeNode root = new TreeNode(nums[mid]);
-        root.left = build(nums, left,mid-1);
+        root.left = build(nums,left,mid-1);
         root.right = build(nums,mid+1,right);
         return root;
     }
