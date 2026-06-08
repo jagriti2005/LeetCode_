b1cc22e7f97bc8383436089653a1556a749b46b5
jagriti2005 <154238507+jagriti2005@users.noreply.github.com>
2026-06-09 00:37:12 +0530

    Time: 2 ms (90.72%) | Memory: 44.4 MB (24.73%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,14 +1,14 @@
 class Solution {
     class Pair{
-        int row;
-        int col;
-        Pair(int row,int col){
-            this.row = row;
-            this.col = col;
+        int r,c;
+        Pair(int r,int c){
+            this.r = r;
+            this.c = c;
         }
     }
-    int rowDirection[] ={1,-1,0,0};
-    int colDirection[] ={0,0,1,-1};
+
+    int[] rowD = {0,0,1,-1};
+    int[] colD = {-1,1,0,0};
 
     public int orangesRotting(int[][] grid) {
         int m = grid.length;
@@ -17,36 +17,36 @@ class Solution {
         boolean[][] vis = new boolean[m][n];
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(grid[i][j] == 2){
+                if(grid[i][j]==2 && !vis[i][j]){
                     q.add(new Pair(i,j));
                     vis[i][j] = true;
                 }
             }
         }
+
         int min = 0;
+
         while(q.size() > 0){
             int size = q.size();
             boolean flag = false;
-            while(size != 0){
-                size--;
-                Pair front = q.remove();
-                int currPairRow = front.row;
-                int currPairCol = front.col;
+            while(size-- > 0){
+                Pair curr = q.poll();
+                int r = curr.r;
+                int c = curr.c;
                 for(int i=0;i<4;i++){
-                    int newRow = currPairRow + rowDirection[i];
-                    int newCol = currPairCol + colDirection[i];
-                    if(newRow>=0 && newRow<m && newCol>=0 && newCol<n){
-                        if(grid[newRow][newCol] == 1 && !vis[newRow][newCol]){
-                            flag = true;
-                            q.add(new Pair(newRow,newCol));
-                            vis[newRow][newCol] = true;
-                            grid[newRow][newCol] = 2;
-                        } 
+                    int nr = rowD[i] + r;
+                    int nc = colD[i] + c;
+                    if(nr>=0 && nr<m && nc>=0 && nc<n && grid[nr][nc]==1 && !vis[nr][nc]){
+                        flag = true;
+                        q.add(new Pair(nr,nc));
+                        vis[nr][nc] = true;
+                        grid[nr][nc] = 2;
                     }
                 }
             }
-            if(flag == true) min++;
+            if(flag==true) min++;
         }
+
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j] == 1) return -1;
