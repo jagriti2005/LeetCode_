cadc511c12f4a4bba8b6d5503c160ecd9d64bd4b
jagriti2005 <154238507+jagriti2005@users.noreply.github.com>
2026-06-09 02:08:33 +0530

    Time: 2 ms (44.92%) | Memory: 48.4 MB (97.61%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,26 +1,34 @@
 class Solution {
     public double findMedianSortedArrays(int[] a, int[] b) {
-        if (a.length > b.length)  return findMedianSortedArrays(b, a);
-        int n = a.length, m = b.length;
-        int low = 0, high = n;
+        int n = a.length;
+        int m = b.length;
 
-        while(low <= high){
-            int cut1 = (low+high)/2;
-            int cut2 = (n+m+1)/2-cut1;
+        int[] arr = new int[n + m];
 
-            int left1 = (cut1==0) ? Integer.MIN_VALUE : a[cut1-1];
-            int left2 = (cut2==0) ? Integer.MIN_VALUE : b[cut2-1];
+        int i = 0, j = 0, k = 0;
 
-            int right1 = (cut1==n) ? Integer.MAX_VALUE : a[cut1];
-            int right2 = (cut2==m) ? Integer.MAX_VALUE : b[cut2];
-
-            if(left1 <= right2 && left2 <= right1){
-                if((n+m)%2 == 0) return (Math.max(left1,left2) + Math.min(right1,right2))/2.0;
-                else return Math.max(left1,left2);
+        while (i < n && j < m) {
+            if (a[i] <= b[j]) {
+                arr[k++] = a[i++];
+            } else {
+                arr[k++] = b[j++];
             }
-            else if(left1 > right2) high = cut1-1;
-            else low = cut1+1;
         }
-        return 0.0;
+
+        while (i < n) {
+            arr[k++] = a[i++];
+        }
+
+        while (j < m) {
+            arr[k++] = b[j++];
+        }
+
+        int len = n + m;
+
+        if (len % 2 == 1) {
+            return arr[len / 2];
+        }
+
+        return (arr[len / 2] + arr[len / 2 - 1]) / 2.0;
     }
 }
