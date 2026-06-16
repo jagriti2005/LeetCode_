37c966c4d04749eee9086670ae6ce777602bb9be
jagriti2005 <154238507+jagriti2005@users.noreply.github.com>
2026-06-16 22:31:50 +0530

    Time: 2 ms (88.58%) | Memory: 47 MB (99.71%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -5,8 +5,7 @@ class Solution {
         for(int i=0;i<n;i++){
             if(i > max) return false;
             max = Math.max(max,i + nums[i]);
-            if(max >= nums.length-1) return true;
         }
-        return false;
+        return true;
     }
 }
