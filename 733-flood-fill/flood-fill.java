a69cdcd27578018b4a4561c4cbc17ef8ea8875b1
jagriti2005 <154238507+jagriti2005@users.noreply.github.com>
2026-06-17 23:24:32 +0530

    Time: 2 ms (11.46%) | Memory: 46.6 MB (77.65%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,4 +1,5 @@
 class Solution {
+    int n,m;
     class Pair{
         int r,c;
         Pair(int r, int c){
@@ -6,29 +7,26 @@ class Solution {
             this.c = c;
         }
     }
-
     int[] rowD = {0,0,-1,1};
     int[] colD = {-1,1,0,0};
 
-    public void bfs(int[][] image, int sr, int sc, int color, boolean[][] vis){
-        int n = image.length;
-        int m = image[0].length;
-
+    public void bfs(int[][] image,int sr,int sc,int color,boolean[][] vis){
         Queue<Pair> q = new LinkedList<>();
         q.add(new Pair(sr,sc));
-        vis[sr][sc] = true;
         int original = image[sr][sc];
         image[sr][sc] = color;
+        vis[sr][sc] = true;
 
-        while(q.size() > 0){
+        while(q.size()>0){
             Pair curr = q.poll();
             int r = curr.r;
             int c = curr.c;
+
             for(int i=0;i<4;i++){
                 int nr = rowD[i] + r;
                 int nc = colD[i] + c;
 
-                if(nr>=0 && nr<n && nc>=0 && nc<m && !vis[nr][nc] && image[nr][nc] == original){
+                if(nr>=0 && nr<n && nc>=0 && nc<m && image[nr][nc]==original && !vis[nr][nc]){
                     q.add(new Pair(nr,nc));
                     vis[nr][nc] = true;
                     image[nr][nc] = color;
@@ -37,10 +35,10 @@ class Solution {
         }
     }
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int n = image.length;
-        int m = image[0].length;
+        n = image.length;
+        m = image[0].length;
         boolean[][] vis = new boolean[n][m];
-        bfs(image, sr,sc,color,vis);
+        bfs(image,sr,sc,color,vis);
         return image;
     }
 }
