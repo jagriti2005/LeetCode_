10edd890777c5adb46ac15778a84043bbd8fcca7
jagriti2005 <154238507+jagriti2005@users.noreply.github.com>
2026-06-17 12:47:03 +0530

    Time: 1 ms (99.71%) | Memory: 47.4 MB (21.00%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.java
+++ 45-jump-game-ii/jump-game-ii.java
@@ -1,16 +1,19 @@
 class Solution {
     public int jump(int[] nums) {
         int n = nums.length;
-        int[] dp = new int[n];
-        Arrays.fill(dp,Integer.MAX_VALUE);
-        dp[0] = 0;
+        int jump = 0;
+        int max = 0;
+        int end = 0;
 
-        for(int i=0;i<n;i++){
-            for(int j=1;j<=nums[i] && i+j < n;j++){
-                dp[i+j] = Math.min(dp[i+j], dp[i]+1);
+        for(int i=0;i<n-1;i++){
+            max = Math.max(max,i+nums[i]);
+
+            if(i==end){
+                jump++;
+                end = max;
             }
         }
 
-        return dp[n-1];
+        return jump;
     }
 }
