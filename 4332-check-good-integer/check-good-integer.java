8a89563f26e273cf69643c232f6067d4626c98f2
jagriti2005 <154238507+jagriti2005@users.noreply.github.com>
2026-07-11 17:18:57 +0530

    Time: 1 ms (95.55%) | Memory: 42.2 MB (94.08%) - LeetSync

---
+++ 4332-check-good-integer/check-good-integer.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean checkGoodInteger(int n) {
+        int digitSum = 0;
+        int squareSum = 0;
+
+        while(n>0){
+            int rem = n%10;
+            digitSum += rem;
+            squareSum += (rem*rem);
+            n /= 10;
+        }
+
+        return squareSum - digitSum >= 50;
+    }
+}
