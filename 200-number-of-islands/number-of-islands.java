9757f0b2e87a8f39630ab580771af6350b46a35d
jagriti2005 <154238507+jagriti2005@users.noreply.github.com>
2026-07-17 16:26:43 +0530

    Time: 6 ms (20.97%) | Memory: 51.5 MB (86.06%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,29 +1,44 @@
 class Solution {
-    public void dfs(char[][] grid, int i, int j, boolean[][] vis){
-        int n = grid.length;
-        int m = grid[0].length;
-
-        if(i<0 || i>= n || j<0 || j>=m || grid[i][j] != '1' || vis[i][j]) return;
-        
+    class Pair{
+        int r,c;
+        Pair(int r, int c){
+            this.r = r;
+            this.c = c;
+        }
+    }
+    int[] rowD = {0,0,-1,1};
+    int[] colD = {1,-1,0,0};
+    public void bfs(int i, int j, char[][] grid, int m ,int n, boolean[][] vis){
         vis[i][j] = true;
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(i,j));
+        while(q.size() > 0){
+            Pair curr = q.poll();
+            int r = curr.r;
+            int c = curr.c;
 
-        dfs(grid,i+1,j,vis);
-        dfs(grid,i,j+1,vis);
-        dfs(grid,i-1,j,vis);
-        dfs(grid,i,j-1,vis);
+            for(int k=0;k<4;k++){
+                int nr = rowD[k] + r;
+                int nc = colD[k] + c;
+
+                if(nr>=0 && nr<m && nc>=0 && nc<n && !vis[nr][nc] && grid[nr][nc] == '1'){
+                    vis[nr][nc] = true;
+                    q.add(new Pair(nr,nc));
+                }
+            }
+        }
     }
     public int numIslands(char[][] grid) {
-        int n = grid.length;
-        int m = grid[0].length;
-        boolean[][] vis = new boolean[n][m];
-        
+        int m = grid.length, n = grid[0].length;
+        boolean[][] vis = new boolean[m][n];
+
         int count = 0;
 
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
                 if(grid[i][j] == '1' && !vis[i][j]){
+                    bfs(i,j,grid,m,n,vis);
                     count++;
-                    dfs(grid,i,j,vis);
                 }
             }
         }
