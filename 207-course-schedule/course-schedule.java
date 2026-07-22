82c411fd2d9d265ff8358429ec62a8988e293f8c
jagriti2005 <154238507+jagriti2005@users.noreply.github.com>
2026-07-22 23:29:48 +0530

    Time: 6 ms (72.03%) | Memory: 46.7 MB (96.42%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,73 +1,33 @@
-// class Solution {
-//     public boolean canFinish(int numCourses, int[][] grid) {
-//         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-//         for(int i=0;i<numCourses;i++){
-//             adj.add(new ArrayList<>());
-//         }
-//         for(int i=0;i<grid.length;i++){
-//             int u = grid[i][0],v=grid[i][1];
-//             adj.get(u).add(v);
-//         }
-//         int[] indeg = new int[numCourses];
-//         for(int i=0;i<numCourses;i++){
-//             for(int ele:adj.get(i)){
-//                 indeg[ele]++;
-//             }
-//         }
-//         Queue<Integer> q = new LinkedList<>();
-//         for(int i=0;i<numCourses;i++){
-//             if(indeg[i]==0){
-//                 q.add(i);
-//             }
-//         }
-//         while(q.size()>0){
-//             int front = q.remove();
-//             for(int ele:adj.get(front)){
-//                 indeg[ele] --;
-//                 if(indeg[ele]==0){
-//                     q.add(ele);
-//                 }
-//             }
-//         }
-//         for(int i=0;i<numCourses;i++){
-//             if(indeg[i] != 0) return false;
-//         }
-//         return true;
-//     }
-// }
-
-
-
-//dfs
 class Solution {
-    public void dfs(int i,ArrayList<ArrayList<Integer>> adj,boolean[] vis,boolean[] path,boolean[] ans){
-        vis[i] = true;
-        path[i] = true;
-        for(int ele:adj.get(i)){
-            if(path[ele]){
-                ans[0] = false;
-                return;
-            }
-            if(!vis[ele]) dfs(ele,adj,vis,path,ans);
-        }
-        path[i] = false;
-    }
-    public boolean canFinish(int numCourses, int[][] grid) {
+    public boolean canFinish(int num, int[][] grid) {
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        for(int i=0;i<numCourses;i++){
-            adj.add(new ArrayList<>());
-        }
-        for(int i=0;i<grid.length;i++){
-            int u = grid[i][0],v=grid[i][1];
+        int[] indeg = new int[num];
+
+        for(int i=0;i<num;i++) adj.add(new ArrayList<>());
+
+        for(int[] a : grid){
+            int u = a[0], v = a[1];
             adj.get(u).add(v);
+            indeg[v]++;
         }
-        boolean[] ans = new boolean[1];
-        ans[0] = true;
-        boolean[] vis = new boolean[numCourses];
-        boolean[] path = new boolean[numCourses];
-        for(int i=0;i<numCourses;i++){
-            if(!vis[i]) dfs(i,adj,vis,path,ans);
+
+        Queue<Integer> q = new LinkedList<>();
+        for(int i=0;i<num;i++){
+            if(indeg[i] == 0) q.add(i);
         }
-        return ans[0];
+
+        while(q.size() > 0){
+            int curr = q.poll();
+            for(int ele:adj.get(curr)){
+                indeg[ele]--;
+                if(indeg[ele] == 0) q.add(ele);
+            }
+        }
+
+        for(int i=0;i<num;i++){
+            if(indeg[i] != 0) return false;
+        }
+
+        return true;
     }
 }
