0fc74766888f21de0410a1cecfd88fedd939ea27
jagriti2005 <154238507+jagriti2005@users.noreply.github.com>
2026-07-29 13:58:17 +0530

    Time: 2 ms (90.04%) | Memory: 43.9 MB (85.18%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,7 +1,7 @@
 class Solution {
     class Pair{
         int r,c;
-        Pair(int r,int c){
+        Pair(int r, int c){
             this.r = r;
             this.c = c;
         }
@@ -13,38 +13,42 @@ class Solution {
     public int orangesRotting(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
+
+        int min = 0;
         Queue<Pair> q = new LinkedList<>();
         boolean[][] vis = new boolean[m][n];
+
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(grid[i][j]==2 && !vis[i][j]){
+                if(grid[i][j] == 2 && !vis[i][j]){
                     q.add(new Pair(i,j));
                     vis[i][j] = true;
                 }
             }
         }
 
-        int min = 0;
-
         while(q.size() > 0){
             int size = q.size();
             boolean flag = false;
+
             while(size-- > 0){
                 Pair curr = q.poll();
                 int r = curr.r;
                 int c = curr.c;
-                for(int i=0;i<4;i++){
-                    int nr = rowD[i] + r;
-                    int nc = colD[i] + c;
-                    if(nr>=0 && nr<m && nc>=0 && nc<n && grid[nr][nc]==1 && !vis[nr][nc]){
-                        flag = true;
+
+                for(int k=0;k<4;k++){
+                    int nr = rowD[k] + r;
+                    int nc = colD[k] + c;
+
+                    if(nr>=0 && nr<m && nc>=0 && nc<n && grid[nr][nc] == 1 && !vis[nr][nc]){
                         q.add(new Pair(nr,nc));
                         vis[nr][nc] = true;
-                        grid[nr][nc] = 2;
+                        grid[nr][nc] = grid[r][c];
+                        flag = true;
                     }
                 }
             }
-            if(flag==true) min++;
+            if(flag == true) min++;
         }
 
         for(int i=0;i<m;i++){
@@ -52,6 +56,6 @@ class Solution {
                 if(grid[i][j] == 1) return -1;
             }
         }
-        return min;
+        return  min;
     }
 }
