ae4da0281c26df3d15c88cd4de5f92489c795ca7
jagriti2005 <154238507+jagriti2005@users.noreply.github.com>
2026-07-29 15:39:12 +0530

    Time: 19 ms (9.44%) | Memory: 49 MB (67.20%) - LeetSync

--- 542-01-matrix/01-matrix.java
+++ 542-01-matrix/01-matrix.java
@@ -7,18 +7,18 @@ class Solution {
         }
     }
 
-    int[] rowD = {0,0,-1,1};
+    int[] rowD = {0,0,1,-1};
     int[] colD = {-1,1,0,0};
 
     public int[][] updateMatrix(int[][] mat) {
-        int n = mat.length;
-        int m = mat[0].length;
-        int[][] ans = new int[n][m];
-        boolean[][] vis = new boolean[n][m];
+        int m = mat.length;
+        int n = mat[0].length;
+        int[][] ans = new int[m][n];
+        boolean[][] vis = new boolean[m][n];
+
         Queue<Pair> q = new LinkedList<>();
-        
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
                 if(mat[i][j] == 0){
                     q.add(new Pair(i,j));
                     vis[i][j] = true;
@@ -26,25 +26,23 @@ class Solution {
                 }
             }
         }
-
+        
         while(q.size() > 0){
             Pair curr = q.poll();
             int r = curr.r;
             int c = curr.c;
 
-            for(int i=0;i<4;i++){
-                int nr = rowD[i] + r;
-                int nc = colD[i] + c;
+            for(int k=0;k<4;k++){
+                int nr = rowD[k] + r;
+                int nc = colD[k] + c;
 
-                if(nr>=0 && nr<n && nc>=0 && nc<m && mat[nr][nc]==1 && !vis[nr][nc]){
+                if(nr>=0 && nr<m && nc>=0 && nc<n && mat[nr][nc] == 1 && !vis[nr][nc]){
                     q.add(new Pair(nr,nc));
                     vis[nr][nc] = true;
                     ans[nr][nc] = ans[r][c] + 1;
                 }
             }
-
         }
-
         return ans;
     }
 }
