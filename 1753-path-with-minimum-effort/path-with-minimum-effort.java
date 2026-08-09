48b6253e51f4dbf9b400a8da2c5689881b551744
jagriti2005 <154238507+jagriti2005@users.noreply.github.com>
2026-08-09 23:32:50 +0530

    Time: 37 ms (96.68%) | Memory: 47.9 MB (42.32%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.java
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.java
@@ -9,9 +9,11 @@ class Solution {
             this.effort=effort;
         }
     }
-    public int minimumEffortPath(int[][] heights) {
-        int n=heights.length;
-        int m=heights[0].length;
+    int[] dr={-1,1,0,0};
+    int[] dc={0,0,-1,1};
+    public int minimumEffortPath(int[][] arr) {
+        int n=arr.length;
+        int m=arr[0].length;
         int[][] dist=new int[n][m];
         for(int[] row:dist){
             Arrays.fill(row,Integer.MAX_VALUE);
@@ -19,8 +21,6 @@ class Solution {
         PriorityQueue<Node> pq= new PriorityQueue<>((a,b)->a.effort-b.effort);
         pq.add(new Node(0,0,0));
         dist[0][0]=0;
-        int[] dr={-1,1,0,0};
-        int[] dc={0,0,-1,1};
         while(pq.size()>0){
             Node cur=pq.poll();
             if(cur.r==n-1 && cur.c==m-1)
@@ -28,13 +28,13 @@ class Solution {
             for(int i=0;i<4;i++){
                 int nr=cur.r+dr[i];
                 int nc=cur.c+dc[i];
-
-                if(nr<0||nc<0||nr>=n||nc>=m) continue;
-                int edge=Math.abs(heights[cur.r][cur.c]-heights[nr][nc]);
-                int newEffort=Math.max(cur.effort,edge);
-                if(newEffort<dist[nr][nc]){
-                    dist[nr][nc]=newEffort;
-                    pq.add(new Node(nr,nc,newEffort));
+                if(nr>=0 && nr<n && nc>=0 && nc<m){
+                    int edge=Math.abs(arr[cur.r][cur.c]-arr[nr][nc]);
+                    int newEffort=Math.max(cur.effort,edge);
+                    if(newEffort < dist[nr][nc]){
+                        dist[nr][nc]=newEffort;
+                        pq.add(new Node(nr,nc,newEffort));
+                    }
                 }
             }
         }
