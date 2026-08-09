eac78b7438923965bb9f742016223934007e09b7
jagriti2005 <154238507+jagriti2005@users.noreply.github.com>
2026-08-09 23:02:04 +0530

    Time: 79 ms (48.81%) | Memory: 47.9 MB (66.66%) - LeetSync

--- 127-word-ladder/word-ladder.java
+++ 127-word-ladder/word-ladder.java
@@ -2,34 +2,39 @@ class Solution {
     class Pair{
         String word;
         int val;
-        Pair(String word,int val){
+        Pair(String word, int val){
             this.word = word;
             this.val = val;
         }
     }
-    public int ladderLength(String beginWord, String endWord, List<String> wordList){
-        Queue<Pair> q = new LinkedList<>();
+    public int ladderLength(String start, String end, List<String> wordList) {
         HashSet<String> set = new HashSet<>(wordList);
-        if(!set.contains(endWord)) return 0;
-        q.add(new Pair(beginWord,1));
-        set.remove(beginWord);
-        while(q.size()>0){
-            Pair front = q.remove();
-            String newWord = front.word;
-            char[] wordArr = newWord.toCharArray();
-            int newVal = front.val;
-            if(newWord.equals(endWord)) return newVal;
-            for(int i=0;i<newWord.length();i++){
-                char original = newWord.charAt(i);
+        Queue<Pair> q = new LinkedList<>();
+
+        if(!set.contains(end)) return 0;
+
+        q.add(new Pair(start, 1));
+        set.remove(start);
+
+        while(q.size() > 0){
+            Pair curr = q.poll();
+            String word = curr.word;
+            char[] arr = word.toCharArray();
+
+            int val = curr.val;
+
+            if(word.equals(end)) return val;
+
+            for(int i=0;i<word.length();i++){
+                char ch = word.charAt(i);
                 for(char j='a';j<='z';j++){
-                    wordArr[i] = j;
-                    String temp = new String(wordArr);
-                    if(set.contains(temp)){
-                        q.add(new Pair(temp,newVal+1));
-                    }
+                    arr[i] = j;
+                    String temp = new String(arr);
+
+                    if(set.contains(temp)) q.add(new Pair(temp, val+1));
                     set.remove(temp);
                 }
-                wordArr[i] = original;
+                arr[i] = ch;
             }
         }
         return 0;
