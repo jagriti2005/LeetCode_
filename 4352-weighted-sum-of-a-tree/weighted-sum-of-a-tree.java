d43ed403dbec934a7ab181d2d403d69f3667d6ec
jagriti2005 <154238507+jagriti2005@users.noreply.github.com>
2026-08-09 14:28:27 +0530

    Time: 44 ms (67.75%) | Memory: 160.5 MB (41.42%) - LeetSync

--- 4352-weighted-sum-of-a-tree/weighted-sum-of-a-tree.java
+++ 4352-weighted-sum-of-a-tree/weighted-sum-of-a-tree.java
@@ -2,45 +2,37 @@ class Solution {
     public long weightedSum(int[] parent, int[] nums) {
         int n = parent.length;
 
-        // Build adjacency list
         ArrayList<Integer>[] tree = new ArrayList[n];
-
-        for (int i = 0; i < n; i++) {
-            tree[i] = new ArrayList<>();
-        }
-
-        for (int i = 1; i < n; i++) {
-            tree[parent[i]].add(i);
-        }
-
-        // Find depth of every node using BFS
         int[] depth = new int[n];
 
         Queue<Integer> q = new LinkedList<>();
-        q.offer(0);
-        depth[0] = 1;
-
         int height = 1;
 
-        while (!q.isEmpty()) {
+        for(int i=0;i<n;i++) tree[i] = new ArrayList<>();
+        for(int i=1;i<n;i++){
+            tree[parent[i]].add(i);
+        }
+
+        q.add(0);
+        depth[0] = 1;
+
+        while(q.size() > 0){
             int node = q.poll();
 
-            for (int child : tree[node]) {
+            for(int child: tree[node]){
                 depth[child] = depth[node] + 1;
-
                 height = Math.max(height, depth[child]);
 
-                q.offer(child);
+                q.add(child);
             }
         }
 
-        // Calculate weighted sum
-        long ans = 0;
+        long weight = 0;
 
-        for (int i = 0; i < n; i++) {
-            ans += (long) nums[i] * (height - depth[i] + 1);
+        for(int i=0;i<n;i++){
+            weight += (long) nums[i] * (height - depth[i] + 1);
         }
 
-        return ans;
+        return weight;
     }
 }
