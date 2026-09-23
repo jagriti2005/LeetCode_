b1ce70ace82cdb1cfddbfe60cd3eb53a9b78f894
jagriti2005 <154238507+jagriti2005@users.noreply.github.com>
2026-09-24 01:07:18 +0530

    Time: 110 ms (5.45%) | Memory: 51.5 MB (5.27%) - LeetSync

--- 303-range-sum-query-immutable/range-sum-query-immutable.java
+++ 303-range-sum-query-immutable/range-sum-query-immutable.java
@@ -1,37 +1,18 @@
 class NumArray {
     int[] arr;
-    int[] segment;
     public NumArray(int[] nums) {
         arr = nums;
-        int n = arr.length;
-        segment = new int[4*n];
-        buildtree(0,0,n-1);
     }
     
-    public void buildtree(int i, int s, int e){
-        if(s==e){
-            segment[i] = arr[s];
-            return;
-        }
-        int mid = (s+e)/2;
-        buildtree(2*i+1,s,mid);
-        buildtree(2*i+2,mid+1,e);
-        segment[i] = segment[2*i+1] + segment[2*i+2];
-    }
-    public int rangeSum(int i, int l, int r, int s, int e){
-        //complete overlap
-        if(l<=s && r>=e) return segment[i];
-
-        //no overlap
-        if(r<s || l>e) return 0;
-
-        // partial overlap
-        int mid = (s+e)/2;
-        return rangeSum(2*i+1,l,r,s,mid) + rangeSum(2*i+2,l,r,mid+1,e);
-    }
     public int sumRange(int left, int right) {
-        int n = arr.length;
-        return rangeSum(0,left,right,0,n-1);
+        int[] prefix = new int[arr.length];
+        prefix[0] = arr[0];
+        for(int i=1;i<arr.length;i++){
+            prefix[i] = arr[i] + prefix[i-1];
+        }
+
+        if(left==0) return prefix[right];
+        return prefix[right] - prefix[left-1];
     }
 }
 
